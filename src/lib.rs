d8219c47717ca7e560be7294776d8debea35c550
oli <47798035+vablings@users.noreply.github.com>
2025-06-28 01:00:53 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -32,44 +32,40 @@ macro_rules! offset {
 }
 
 
-#[macro_export]
-/// Same as offset except using custom Debug implementation that behaves
-/// like the derived Debug implementation for this struct with the exception
-/// that the padding fields are not printed
-macro_rules! offset_debug {
-    // Guarded munched exprs
-    // output
-    (@guard ($current_offset:expr) -> {$(#[$attr:meta])* $vis:vis struct $name:ident $(($amount:expr, $vis_field:vis $id:ident: $ty:ty))*}) =>    {
+#![no_std]
+pub extern crate paste;
 
+#[macro_export]
+macro_rules! offset_debug {
+    // Final struct definition
+    (@build ($current_offset:expr) -> { $(#[$attr:meta])* $vis:vis struct $name:ident $(($pad_amount:expr, $vis_field:vis $id:ident : $ty:ty))* }) => {
         $crate::paste::paste! {
             #[repr(C, packed)]
-            $(#[$attr])* $vis struct $name { $([<_pad $id>]: [u8;$amount], $vis_field $id: $ty),* }
-        }
-
-        impl core::fmt::Debug for $name {
-            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-                f.debug_struct(stringify!($name))
-                 $(.field(stringify!($id), unsafe { &core::ptr::read_unaligned(core::ptr::addr_of!(self.$id))}))*
-                 .finish()
+            $(#[$attr])* $vis struct $name {
+                $([<_pad_ $id>]: [u8; $pad_amount], $vis_field $id : $ty),*
             }
         }
     };
 
-    // add more fields
-    (@guard ($current_offset:expr, $offset:literal $vis_field:vis $id:ident: $ty:ty $(,)?) -> {$($output:tt)*}) => {
-        offset_debug!(@guard ($offset + core::mem::size_of::<$ty>()) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
+    // Add more fields with const/expr offset
+    (@build ($current_offset:expr, $offset:expr $vis_field:vis $id:ident : $ty:ty $(,)?) -> { $($output:tt)* }) => {
+        offset_debug!(@build ($offset + core::mem::size_of::<$ty>()) -> {
+            $($output)* ($offset - $current_offset, $vis_field $id : $ty)
+        });
     };
 
-    (@guard ($current_offset:expr, $offset:literal $vis_field:vis $id:ident: $ty:ty, $($next:tt)+) -> {$($output:tt)*}) => {
-        offset_debug!(@guard ($offset + core::mem::size_of::<$ty>(), $($next)+) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
+    (@build ($current_offset:expr, $offset:expr $vis_field:vis $id:ident : $ty:ty, $($rest:tt)+) -> { $($output:tt)* }) => {
+        offset_debug!(@build ($offset + core::mem::size_of::<$ty>(), $($rest)+) -> {
+            $($output)* ($offset - $current_offset, $vis_field $id : $ty)
+        });
     };
 
-    // Entry points
-    ($(#[$attr:meta])* $vis:vis struct $struct_name:ident {$($input:tt)*}) => {
-        offset_debug!(@guard (0, $($input)*) -> {$(#[$attr])* $vis struct $struct_name});
-        $crate::offset_checker!($struct_name {$($input)*});
+    // Entry point
+    ($(#[$attr:meta])* $vis:vis struct $name:ident { $($body:tt)* }) => {
+        offset_debug!(@build (0, $($body)*) -> { $(#[$attr])* $vis struct $name });
     };
 }
+
 
 #[cfg(feature = "checked")]
 #[macro_export]
